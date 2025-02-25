6b7efd038d8eee3b07cf24e36834ee8505764ae9
badamnarendrareddy <99220041116@klu.ac.in>
2025-02-25 12:42:48 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import {  Route, Switch } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next'; // Import I18nextProvider
 import i18n from './i18n'; // Import your i18n configuration
 
